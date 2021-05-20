8d5948aa0272d379d9d7b5d47ad68d0a0462b573
markirius <markbombomb@gmail.com>
2021-05-20 16:49:34 -0300

    most commented posts as a inclusion_tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -17,8 +17,9 @@ def show_latest_posts(count=5):
     return {"latest_posts": latest_posts}
 
 
-@register.simple_tag
-def get_most_commentd_posts(count=5):
-    return Post.published.annotate(
-                total_comments=Count("comments")
-            ).order_by("-total_comments")[:count]
+@register.inclusion_tag("blog/most_commented_posts.html")
+def show_most_commented_posts(count=5):
+    most_commented_posts = Post.published.annotate(
+                    total_comments=Count("comments")
+                ).order_by("-total_comments")[:count]
+    return {"most_commented_posts": most_commented_posts}
